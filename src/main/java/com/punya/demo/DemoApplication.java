ce9cffb5eae3758370a84eeddba8a09edee32736
punya0908 <punya0908@gmail.com>
2026-07-21 10:42:42 +0530

    frontend added

--- src/main/java/com/punya/demo/DemoApplication.java
+++ src/main/java/com/punya/demo/DemoApplication.java
@@ -30,7 +30,7 @@ public class DemoApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
+						.allowedOrigins("http://localhost:63342")
 						.allowedMethods("*")
 						.allowedHeaders("*");
 			}
